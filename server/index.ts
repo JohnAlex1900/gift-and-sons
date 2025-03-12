b7bb583e23cfc7b9d0c62bb3054b4000ae1d2e18
John Irungu <johnirungu977@gmail.com>
2025-03-12 22:36:28 +0300

    Server Configured

--- server/index.ts
+++ server/index.ts
@@ -76,7 +76,7 @@ app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
 });
 
 // ✅ Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
