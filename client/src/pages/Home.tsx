2bcb148544dbc8bc9d62788c60b3e2995e0564c8
John Irungu <johnirungu977@gmail.com>
2025-03-14 16:46:47 +0300

    Vercel Backend

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,6 +4,8 @@ import { useQuery } from "@tanstack/react-query";
 import type { Property } from "@shared/schema";
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export default function Home() {
   // Fetch featured properties
   const {
@@ -14,7 +16,9 @@ export default function Home() {
   } = useQuery<Property[]>({
     queryKey: ["/api/properties/featured"],
     queryFn: async () => {
-      const response = await axios.get<Property[]>("/api/properties/featured");
+      const response = await axios.get<Property[]>(
+        `${API_URL}/properties/featured`
+      );
       return response.data;
     },
   });
